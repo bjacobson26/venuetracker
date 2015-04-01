c1e0fdffe1a59f92b7dcd0b1f3c8cd76f6388713
Bo Jacobson <bjacobson26@gmail.com>
2015-04-01 10:02:15 -0700

    button fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,20 @@
 class EventsController < ApplicationController
   def index
   	@events = Event.all
+
+    if params[:search].present?
+      @events = Event.where("name ilike ?", "%#{params[:search]}%")
+    else
+      @events = Event.all
+    end
+
+    respond_to do |format|
+      format.html{}
+      format.json{ render json: @events.map(&:name) }
+    end
+
+
+
   end
 
   
